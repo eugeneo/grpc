d3cf7623e3bf7a97658e5348acdfc8c3cb06e25e
Eugene Ostroukhov <eostroukhov@google.com>
2024-12-27 13:38:20 -0800

    fixups

--- test/core/event_engine/posix/posix_system_api_test.cc
+++ test/core/event_engine/posix/posix_system_api_test.cc
@@ -32,6 +32,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
 #include <memory>
 #include <utility>
 
@@ -347,29 +348,53 @@ TEST(PosixSystemApiTest, DISABLED_FullStopBeforeFork) {
   EXPECT_TRUE(status.ok()) << status.error_message();
 }
 
-TEST(PosixSystemApiTest, NoGrpcBeforeFork) {
+namespace {
+
+absl::StatusOr<std::pair<int, int>> StartServerGetPort() {
   std::array<int, 2> pipe_fds;
-  ASSERT_GE(pipe(pipe_fds.data()), 0)
-      << absl::ErrnoToStatus(errno, "Creating pipe");
+  if (pipe(pipe_fds.data()) != 0) {
+    return absl::ErrnoToStatus(errno, "Creating pipe");
+  }
   int pid = fork();
-  ASSERT_GE(pid, 0) << absl::ErrnoToStatus(errno, "Fork");
+  if (pid < 0) {
+    return absl::ErrnoToStatus(errno, "Fork");
+  }
   if (pid == 0) {
     int port = grpc_pick_unused_port_or_die();
     close(pipe_fds[0]);
-    ASSERT_EQ(write(pipe_fds[1], &port, sizeof(port)), sizeof(port))
-        << absl::ErrnoToStatus(errno, "Writing port");
+    if (write(pipe_fds[1], &port, sizeof(port)) < 0) {
+      std::cerr << absl::ErrnoToStatus(errno, "Writing port") << "\n";
+      exit(1);
+    }
     close(pipe_fds[1]);
-    LOG(INFO) << "Child PID: " << getpid() << " port " << port;
-    ASSERT_THAT(ExecServer(port), IsOk());
+    std::cout << "Child PID: " << getpid() << " port " << port << "\n";
+    absl::Status server_status = ExecServer(port);
+    if (!server_status.ok()) {
+      std::cerr << server_status << "\n";
+      exit(1);
+    }
   }
   close(pipe_fds[1]);
   int port;
   int r = 0;
   while (r < sizeof(port)) {
     int rd = read(pipe_fds[0], &port + r, sizeof(port) - r);
-    ASSERT_GE(rd, 0) << absl::ErrnoToStatus(errno, "Reading the pipe");
+    if (rd < 0) {
+      return absl::ErrnoToStatus(errno, "Reading the pipe");
+    }
     r += rd;
   }
+  close(pipe_fds[0]);
+  return std::make_pair(port, pid);
+}
+
+}  // namespace
+
+TEST(PosixSystemApiTest, NoGrpcBeforeFork) {
+  absl::StatusOr<std::pair<int, int>> port_pid = StartServerGetPort();
+  ASSERT_TRUE(port_pid.ok()) << port_pid.status();
+  int port = port_pid->first;
+  int pid = port_pid->second;
   grpc_init();
   LOG(INFO) << "Parent pid: " << getpid() << " port: " << port;
   auto cleanup = absl::MakeCleanup([pid]() { ShutdownChild(pid); });
